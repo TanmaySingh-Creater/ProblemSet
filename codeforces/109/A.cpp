60b312c3c1131f6093df72c9cddf0fd61bae5413
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-02-11 22:35:00 +0530

    Add solution for problem `A - Lucky Sum of Digits` Link: https://codeforces.com/contest/109/problem/A Tags: brute force, implementation, *1000 Ref: https://codeforces.com/contest/109/submission/70801569

--- codeforces/109/A.cpp
+++ codeforces/109/A.cpp
@@ -3,73 +3,81 @@
 //
 //  Created by Tanmay Singh on 21/12/19.
 //  Copyright © 2019 Tanmay Singh. All rights reserved.
+//
 #include <iostream>
 #include <cmath>
 #include <algorithm>
 #include <vector>
-#include <cstring>
+#include <string>
 #include <set>
 #include <map>
 #include <cstdio>
 #include <iterator>
-#include <string>
-#include <stack>
-#include <deque>
-#include <unordered_set>
-#include <unordered_map>
- 
-#define FAST(); ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define endl "\n"
+
+
 #define F first
 #define S second
 #define PB push_back
 #define MP make_pair
-#define MAX 1000000
-#define loopchalao(i , N) for( int i = 0 ; i < N ; i++ )
+#define MAX 100001
 typedef long long  int  bigint ;
-using namespace std ;
-void permute( string str , int l ){
-    if( l == str.length() - 1 ){
-        cout << str << endl ;
+using namespace std;
+
+
+void permute ( string a , int l , int r ){
+    
+    if( l == r ){
+        cout << a << endl ;
     }
-    for( int i = l ; i < str.length() ; i++ ){
-        swap( str[l] , str[i] ) ;
-        permute(str, l + 1 ) ;
-        swap( str[i] , str[l] ) ;
-    }
+    else
+        for( int i = l ; i <= r ; i++ ){
+            swap( a[l] , a[i] ) ;
+            permute ( a , l + 1 , r );
+            swap( a[l] , a[i] );   //backtracking
+        }
+    
 }
-    void findMin( int sum ){
-        int fours = 0, sevens = 0;
-        while (sum > 0){
-            if (sum % 7 == 0){
-                sevens++;
-                sum -= 7;
-            }
-            else if (sum % 4 == 0){
-                fours++;
-                sum -= 4;
-            }
-            else{
-                fours++ ; sum -= 4;
-            }
+
+
+
+
+
+int main(){
+    
+   // 4 + 4 + 4 + 7
+    int N ;
+    cin >> N ;
+    int seven = 0 ;
+    int four = 0 ;
+    while( N > 0 ){
+        if( N % 7 == 0 ){
+            seven++ ;
+            N -= 7 ;
         }
-        if (sum < 0){
-            cout << "-1" << endl ;
-            return ;
+        else if( N % 4 == 0 ){
+            four ++ ;
+            N -= 4 ;
         }
-        for (int i = 0 ; i < fours ; i++){
-            cout << "4" ;
-        }
-      
-        for (int i=0 ; i < sevens ; i++){
-            cout << "7" ;
+        else{
+            N -= 4 ;
+            four++ ;
         }
     }
-int main() {
-    //freopen("input.txt","r",stdin);
-    //freopen("output.txt","w",stdout);
-    int N ; cin >> N ;
-    findMin( N ) ;
-    cout << endl ;
+    if( N < 0 ){
+        cout <<"-1"<<endl;
+    }
+    else
+    {
+        for( int i = 0 ; i < four ; i++ ){
+            cout << "4";
+        }
+        for(int i = 0 ; i < seven ; i++ ){
+            cout << "7";
+        }
+        cout << endl ;
+        
+        
+    }
     return 0 ;
 }
+// 4 7 44 47 74 77 444 447 477 744
