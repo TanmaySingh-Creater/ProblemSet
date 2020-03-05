7362eecbfb23c0d339e40030b0ea8067f0bfb165
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-03-05 21:11:00 +0530

    Add solution for problem `C - Kuroni and Impossible Calculation` Link: https://codeforces.com/contest/1305/problem/C Tags: brute force, combinatorics, math, number theory, *1600 Ref: https://codeforces.com/contest/1305/submission/72523445

---
+++ codeforces/1305/C.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <cmath>
+using namespace std ;
+
+
+int main(){
+    long long int N , M ;
+    cin >> N >> M ;
+    long long int *arr = new long long  int[N] ;
+    for( int i = 0 ; i < N ; i++ ){
+        cin >> arr[i] ;
+    }
+    
+    if( M < N ){
+        cout <<"0"<< endl ;
+        delete []arr;
+        return 0 ;
+    }
+    long long int pro = 1 ;
+    for( int i = 0 ; i < N ; i++ ){
+        for( int j = i + 1  ; j < N ; j++ ){
+            pro = (pro  * abs( arr[i] - arr[j] )) % M ;
+        }
+    }
+    cout << pro << endl ;
+    delete []arr;
+    return 0 ;
+}
