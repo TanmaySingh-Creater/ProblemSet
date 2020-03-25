39df04918c841ab2ed6fcffcdfa0cfbfe5a3774b
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-03-25 13:44:00 +0530

    Add solution for problem `C - Alphabetic Removals` Link: https://codeforces.com/contest/999/problem/C Tags: implementation, *1200 Ref: https://codeforces.com/contest/999/submission/74267617

--- codeforces/999/C.cpp
+++ codeforces/999/C.cpp
@@ -1,43 +1,33 @@
 #include <iostream>
 #include <set>
 #include <map>
+#include <algorithm>
 using namespace std;
- 
+
 int main()
 {
- 
-    int N , steps ;
-    cin >> N >> steps ;
+
+    int N , K ;
+    cin >> N >> K  ;
     string str ;
     cin >> str ;
-    int ch[26] = { 0 } ;
-    for( int i = 0 ; i < N ; i++ ){
-        ch[str[i] - 'a'] ++ ;
-    }
-    map<char , int> m ;
-    for( int i = 0 ; i < 26 ; i++ ){
-        if( ch[i] > 0 && steps > 0 ){
-            if( ch[i] >= steps ){
-                m[char( i + 'a')] = steps ;
-                steps = 0 ;
-                break ;
-            }
-            else{
-                 m[char( i + 'a')] = ch[i] ;
-                steps -= ch[i] ;
-            }
-        }
-    }
- 
+    string abc = str ;
+    sort( abc.begin() , abc.end() );
     
+    map< char , int > m ;
+    for( int i = 0 ; i < K ; i++ ){
+        m[abc[i]] ++ ;
+    }
     for( int i = 0 ; i < str.length() ; i++ ){
-        if( m[str[i]] > 0 ){
-            -- m[str[i]] ;
+        if( m.find(str[i]) != m.end() && m[str[i]] != 0 ){
+            m[str[i]] -- ;
         }
         else{
             cout << str[i] ;
         }
     }
+    
+    
     return 0;
 }
- 
+// t a n m a y
