7e920793853ae289a89f89976ede6cc51d991a3c
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-03-26 22:58:00 +0530

    Add solution for problem `B - Phone numbers` Link: https://codeforces.com/contest/25/problem/B Tags: implementation, *1100 Ref: https://codeforces.com/contest/25/submission/74490455

---
+++ codeforces/25/B.cpp
@@ -0,0 +1,48 @@
+//
+//  CPlibrary
+//
+//  Created by Tanmay Singh on 21/12/19.
+//  Copyright © 2019 Tanmay Singh. All rights reserved.
+//
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <iterator>
+#include <string>
+#include <stack>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+ 
+#define FAST(); ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl "\n"
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MAX 1000001
+#define loopchalao(i , N) for( int i = 0 ; i < N ; i++ )
+typedef long long  int  bigint ;
+
+using namespace std ;
+   
+  
+int main(){
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    FAST();
+    int N ; cin >> N ;
+    string str ; cin >> str ;
+        for( int i = 0 ; i < N ; i++ ){
+            if( i % 2 == 0 && i != N - 1 && i != 0 ){
+                cout <<"-" ;
+            }
+            cout << str[i] ;}
+    return 0 ;
+}
+
