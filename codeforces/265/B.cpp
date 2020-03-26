564d401cff820e99be0527931e0b2c826cb37dcb
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-03-26 22:23:00 +0530

    Add solution for problem `B - Roadside Trees (Simplified Edition)` Link: https://codeforces.com/contest/265/problem/B Tags: greedy, implementation, *1000 Ref: https://codeforces.com/contest/265/submission/74484815

---
+++ codeforces/265/B.cpp
@@ -0,0 +1,59 @@
+//
+//  CPlibrary
+//
+//  Created by Tanmay Singh on 21/12/19.
+//  Copyright © 2019 Tanmay Singh. All rights reserved.
+//
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <iterator>
+#include <string>
+#include <stack>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+ 
+#define FAST(); ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl "\n"
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MAX 1000001
+#define loopchalao(i , N) for( int i = 0 ; i < N ; i++ )
+typedef long long  int  bigint ;
+
+using namespace std ;
+   
+  
+int main(){
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    FAST();
+    int N ; cin >> N ;
+    int arr[N] ;
+    for( int i = 0 ; i < N ; i++ ){
+        cin >> arr[i] ;
+    }
+    bigint ans = N + arr[0] ;
+    
+    for( int i = 1 ; i < N ; i++ ){
+        if( arr[i] < arr[i - 1]){
+            ans += ( bigint )arr[i-1] - arr[i] + 1 ;
+        }
+        else{
+            ans += ( bigint )arr[i] - arr[i - 1 ] + 1 ;
+        }
+    }
+    cout << ans << endl ;
+    
+    
+    return 0 ;
+}
+
