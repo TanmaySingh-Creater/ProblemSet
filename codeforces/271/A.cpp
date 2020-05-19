dadffaf807272cc4caa99f56f8643645d33f7775
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-05-19 18:52:00 +0530

    Add solution for problem `A - Beautiful Year` Link: https://codeforces.com/contest/271/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/271/submission/80691499

---
+++ codeforces/271/A.cpp
@@ -0,0 +1,73 @@
+//
+//  CPlibrary
+//
+//  Created by Tanmay Singh on 3/03/2020.
+// INDIAN INSTITUTE OF TECHNOLOGY (BHU) , VARANASI
+//  Copyright © 2020 Tanmay Singh. All rights reserved.
+//#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+ 
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <climits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+ 
+#define FAST(); ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl "\n"
+#define mod 1000000007
+typedef long long int  bigint ;
+typedef std::vector< int > vi;
+typedef std::vector< long long int > vll;
+typedef std::vector< vi > vvi;
+
+using namespace std;
+
+bool isDistinct( int N ){
+    set<int> s ;
+    while( N > 0 ){
+        int d = N % 10 ;
+        s.insert(d) ;
+        N /= 10 ;
+    }
+    if( s.size() == 4 )
+        return true ;
+    return false ;
+}
+int main(){
+    FAST() ;
+    int N ; cin >> N ;
+    for( int i = N + 1 ;  ; i++ ){
+        if( isDistinct(i) ){
+            cout << i << endl ;
+            return 0 ;
+        }
+    }
+    return 0 ;
+}
