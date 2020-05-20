6239137dfe65f6b7d1bcb6777a41d91541ecf663
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-05-20 21:12:00 +0530

    Add solution for problem `B - Unary` Link: https://codeforces.com/contest/133/problem/B Tags: implementation, *1200 Ref: https://codeforces.com/contest/133/submission/80786915

---
+++ codeforces/133/B.cpp
@@ -0,0 +1,64 @@
+//
+//  CPlibrary
+//
+//  Created by Tanmay Singh on 3/03/2020.
+// INDIAN INSTITUTE OF TECHNOLOGY (BHU) , VARANASI
+//  Copyright © 2020 Tanmay Singh. All rights reserved.
+//#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+ 
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <climits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+ 
+#define FAST(); ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl "\n"
+#define mod 1000003
+typedef long long int  bigint ;
+typedef std::vector< int > vi;
+typedef std::vector< long long int > vll;
+typedef std::vector< vi > vvi;
+
+using namespace std;
+
+int main(){
+    FAST() ;
+    map<char , int> m ;
+    m['>'] = 8 ; m['<'] = 9 ; m['+'] = 10 ; m['-'] = 11 ;
+    m['.'] = 12 ; m[','] = 13 ; m['['] = 14 ; m[']'] = 15 ;
+    string str ; cin >> str ;
+    bigint ans = 0 ;
+    for( int i = 0  ; i < str.length() ; i++ ){
+        ans = ((ans * 16) % mod  + m[str[i]]) % mod  ;
+    }
+    cout << ans << endl ;
+    return 0 ;
+}
