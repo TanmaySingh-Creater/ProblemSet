e5e5eac2d6b18ab08366e45cdc91828f04a5627b
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-05-22 15:46:00 +0530

    Add solution for problem `A - I'm bored with life` Link: https://codeforces.com/contest/822/problem/A Tags: implementation, math, number theory, *800 Ref: https://codeforces.com/contest/822/submission/80923572

---
+++ codeforces/822/A.cpp
@@ -0,0 +1,64 @@
+//
+//  CPlibrary
+//
+//  Created by Tanmay Singh on 3/03/2020.
+// INDIAN INSTITUTE OF TECHNOLOGY (BHU) , VARANASI
+//  Copyright © 2020 Tanmay Singh. All rights reserved.
+//#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+ 
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <climits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+ 
+#define FAST(); ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl "\n"
+#define mod 1000000007
+typedef long long int  bigint ;
+typedef std::vector< int > vi;
+typedef std::vector< long long int > vll;
+typedef std::vector< vi > vvi;
+
+using namespace std;
+bigint factorial( int N ){
+    if( N == 1 || N == 0 )
+        return 1 ;
+    bigint ans = 1 ;
+    for(bigint i = 2 ; i <= N ; i++ )
+        ans *= i ;
+    return ans ;
+}
+int main(){
+    FAST() ;
+    int A , B ; cin >> A >> B ;
+    cout << factorial(min( A , B ) ) << endl ;
+    return 0 ;
+}
