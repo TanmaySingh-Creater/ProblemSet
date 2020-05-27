29e4fd9afa7faebd98cfbd7a5a387a406b4db01c
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-05-27 19:37:00 +0530

    Add solution for problem `B - Sereja and Suffixes` Link: https://codeforces.com/contest/368/problem/B Tags: data structures, dp, *1100 Ref: https://codeforces.com/contest/368/submission/81626316

---
+++ codeforces/368/B.cpp
@@ -0,0 +1,74 @@
+//
+//  CPlibrary
+//
+//  Created by Tanmay Singh on 3/03/2020.
+// INDIAN INSTITUTE OF TECHNOLOGY (BHU) , VARANASI
+//  Copyright © 2020 Tanmay Singh. All rights reserved.
+//#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+ 
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <climits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+ 
+#define FAST(); ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl "\n"
+#define mod 1000000009
+typedef long long int  bigint ;
+typedef std::vector< int > vi;
+typedef std::vector< long long int > vll;
+typedef std::vector< vi > vvi;
+ 
+using namespace std;
+
+int main(){
+    FAST() ;
+    int N , Q ; cin >> N >> Q ;
+    int arr[N+1] ;
+    for( int i = 1 ; i <= N ; i++ )
+        cin >> arr[i] ;
+    set<int> s ;
+    int dp[N+1] ;
+    dp[N] = 1 ; s.insert(arr[N]) ;
+    for( int i = N - 1 ; i >= 1 ; i-- ){
+        if( s.find(arr[i]) != s.end() )
+            dp[i] = dp[i+1] ;
+        else{
+            dp[i] = dp[i+1] + 1 ;
+            s.insert(arr[i]) ;
+        }
+    }
+    while( Q-- ){
+        int X ; cin >> X ;
+        cout << dp[X] << endl ;
+    }
+    return 0 ;
+}
