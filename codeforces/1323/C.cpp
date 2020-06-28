1625da7ce8eb082dc8f01b0bfb98d57aceb25241
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-06-28 22:11:00 +0530

    Add solution for problem `C - Unusual Competitions` Link: https://codeforces.com/contest/1323/problem/C Tags: constructive algorithms, *1300 Ref: https://codeforces.com/contest/1323/submission/85386030

---
+++ codeforces/1323/C.cpp
@@ -0,0 +1,71 @@
+//
+//  CPlibrary
+//
+//  Created by Tanmay Singh on 3/03/2020.
+// INDIAN INSTITUTE OF TECHNOLOGY (BHU) , VARANASI
+//  Copyright Â© 2020 Tanmay Singh. All rights reserved.
+//#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+ 
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <climits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+ 
+#define FAST(); ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl "\n"
+#define MOD 1000000007
+ 
+typedef std::vector< int > vi;
+typedef std::vector< long long int > vll;
+typedef std::vector< vi > vvi;
+typedef long long int bigint ;
+using namespace std;
+int main(){
+    int N ; cin >> N ;
+    string str ; cin >> str ;
+    int cnt = 0 , pref = 0 ;
+    for( int i = 0 ; i < N ; i++ ){
+        if( str[i] == '(' )
+            pref++ ;
+        else
+            pref-- ;
+        if( pref < 0 )
+            cnt++ ;
+        else if( pref == 0 && str[i] == '(' )
+            cnt++ ;
+    }
+    
+    if( pref == 0 )
+        cout << cnt << endl ;
+    else
+        cout << "-1" << endl ;
+    return 0 ;
+}
