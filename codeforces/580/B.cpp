e312b5c72d160fdbe6152659af40b55c392cae4a
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-11-07 16:41:00 +0530

    Add solution for problem `B - Kefa and Company` Link: https://codeforces.com/contest/580/problem/B Tags: binary search, sortings, two pointers, *1500 Ref: https://codeforces.com/contest/580/submission/97838886

--- codeforces/580/B.cpp
+++ codeforces/580/B.cpp
@@ -6,12 +6,15 @@
 #define endl "\n"
 #define deb(x) cout << #x << " " << x << endl ;
 #define PI 3.14159265358979323846264338327
-
-
+#include <vector> 
 using namespace std ;
 using bigint = long long int ;
 using vi = vector<int> ;
 using vll = vector<long long int> ;
+
+bigint pref[100005] ;
+
+
 
 int main(){
     FAST_IO ;
@@ -20,24 +23,23 @@ int main(){
     for( int i = 0 ; i < N ; i++ ){
         cin >> v[i].first >> v[i].second ;
     }
-    
     sort( v.begin() , v.end() ) ;
     
-    bigint ans = 0 , sum = 0 ;
-    
-    int currMin = v[0].first ;
-    
-    for( int i = 0 , j = 0 ; i < N ; i++ ){
-        sum = sum + v[i].second ;
-        while( currMin + M <= v[i].first ){
-            sum -= v[j].second ;
-            j++ ;
-            currMin = v[j].first ;
-        }
-        ans = max( ans , sum ) ;
+    pref[0] = v[0].second ;
+    for( int i = 1 ; i < N ; i++ ){
+        pref[i] = pref[i-1] + v[i].second ;
     }
-    
+  
+    bigint ans = 0 ;
+    for( int i = 0 ; i < N ; i++ ){
+        pair<int, int> p = { v[i].first + M , -10000} ;
+        auto idx = lower_bound(v.begin(), v.end(), p ) - v.begin() ;
+        if( i == 0 ){
+            ans = max( ans , pref[idx-1] ) ;
+        }
+        else
+            ans = max( ans , pref[idx-1] - pref[i-1] ) ;
+    }
     cout << ans << endl ;
-    
     return 0 ;
 }
