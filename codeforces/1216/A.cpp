0dc2a3aac966c8f8ad6553e6a0f29d7218af07b2
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-12-01 14:32:00 +0530

    Add solution for problem `A - Prefixes` Link: https://codeforces.com/contest/1216/problem/A Tags: strings, *800 Ref: https://codeforces.com/contest/1216/submission/100096909

--- codeforces/1216/A.cpp
+++ codeforces/1216/A.cpp
@@ -1,32 +1,36 @@
-#include<bits/stdc++.h>
-#define ll long long
-#define fastio ios_base::sync_with(false); cin.tie(0);cout.tie(0);
-const int MOD = 1000000007;
-#define PI = acos(-1)
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define FAST_IO  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define mod 1000000007
+#define endl "\n"
+#define deb(x) cout << #x << " " << x << endl ;
+#define PI 3.14159265358979323846264338327
+
 using namespace std ;
+using bigint = long long int ;
+using vi = vector<int> ;
+using vll = vector<long long int> ;
+
 int main(){
-    int n ; 
-    cin>> n ; 
-    string s ; cin >> s ;
-    int cnta =0 , cntb=0;
-    for(auto x : s){
-    	if(x=='a') cnta++;
-    	else if(x=='b')cntb++;
-	}
-	int ans = 0 ;
-	for(int i = 0 ; i < n ; i+=2){
-		 if(s[i]!=s[i+1]) continue ;
-		 else {
-		 	if(s[i]=='a'){
-		 		s[i+1] = 'b';
-		 		ans++;
-			 }
-			 else {
-			 	s[i+1]='a';
-			 	ans++;
-			 }
-		 }
-	}
-	cout << ans << endl;
-	cout  << s << endl;
+    FAST_IO ;
+    int N ; cin >> N ;
+    
+    string str ; cin >> str ;
+    
+    int cnt = 0 ;
+    
+    for( int i = 0 ; i < N ; i += 2 ){
+        if( str[i] == 'a' && str[i+1] == 'a' ){
+            str[i] = 'b' ;
+            cnt++ ;
+        }
+        else if( str[i] == 'b' && str[i+1] == 'b' ){
+            cnt++ ;
+            str[i] = 'a' ;
+        }
+    }
+    
+    cout << cnt << endl << str << endl ;
+    return 0 ;
 }
