46dc89d9e18ddc394e0859399f204bb3eaf26155
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-12-01 14:42:00 +0530

    Add solution for problem `B - Shooting` Link: https://codeforces.com/contest/1216/problem/B Tags: greedy, implementation, sortings, *900 Ref: https://codeforces.com/contest/1216/submission/100097641

--- codeforces/1216/B.cpp
+++ codeforces/1216/B.cpp
@@ -1,37 +1,28 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#include <bits/stdc++.h>
-#define FAST_IO  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define mod 1000000007
-#define endl "\n"
-#define deb(x) cout << #x << " " << x << endl ;
-#define PI 3.14159265358979323846264338327
-
+#include<bits/stdc++.h>
+#define ll long long
+#define fastio ios_base::sync_with(false); cin.tie(0);cout.tie(0);
+const int MOD = 1000000007;
+#define PI = acos(-1)
 using namespace std ;
-using bigint = long long int ;
-using vi = vector<int> ;
-using vll = vector<long long int> ;
-
 int main(){
-    FAST_IO ;
-    int N ; cin >> N ;
-    vector<pair<int, int>> v ( N ) ;
-    
-    for( int i = 0 ; i < N ; i++ ){
-         cin >> v[i].first ;
-        v[i].second = i + 1 ;
-    }
-    
-    sort(v.begin(), v.end()) ;
-    reverse(v.begin(), v.end()) ;
-    int ans = 0 ;
-    for( int i = 0 ; i < N ; i++ ){
-        ans = ans + (i * v[i].first + 1 ) ;
-    }
-    cout << ans << endl ;
-    for( auto x : v )
-        cout << x.second << " " ;
-    cout << endl ;
-    return 0 ;
+    int n ; cin >> n ; 
+    vector<pair<int,int>> v;
+    for(int i =0 ; i < n ; i++){
+    	int x; cin >> x ;
+    	 v.push_back({x , i+1});
+	}
+	sort(v.begin() , v.end());
+	reverse(v.begin() , v.end());
+	int x = 0 ; 
+	vector<int> ans1 ; 
+	ll ans = 0 ; 
+	for(int i = 0 ; i < n ; i++){
+		 ans += v[i].first*x+1;
+		 ans1.push_back(v[i].second);
+		 x++;
+	}
+	cout << ans << endl;
+	for(auto x : ans1){
+		cout <<  x << " ";
+	}
 }
-
