66a07bbedc2368343f678d42e1dea6ac935dfadb
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-12-01 15:29:00 +0530

    Add solution for problem `D - Swords` Link: https://codeforces.com/contest/1216/problem/D Tags: math, *1300 Ref: https://codeforces.com/contest/1216/submission/100100852

--- codeforces/1216/D.cpp
+++ codeforces/1216/D.cpp
@@ -1,25 +1,39 @@
-#include<bits/stdc++.h>
-#define ll long long
-#define fastio ios_base::sync_with(false); cin.tie(0);cout.tie(0);
-const int MOD = 1000000007;
-#define PI = acos(-1)
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define FAST_IO  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define mod 1000000007
+#define endl "\n"
+#define deb(x) cout << #x << " " << x << endl ;
+#define PI 3.14159265358979323846264338327
+
 using namespace std ;
-int main(){
-   int n ;cin >> n ; 
-   vector<int> v(n);
-   int ans = 0 ; 
-   for(int i =0 ; i < n ; i++){
-   	    cin >> v[i];
-   	    ans = max(v[i] ,ans);
-   }
-   int val = 0 ; 
-   for(int i = 0 ; i < n ; i++){
-   	   val = __gcd(val , (ans-v[i]));
-   }
-   int z = val ;
-   ll sum = 0 ; 
-   for(int i = 0 ; i < n ; i++){
-   	   sum += (ans-v[i])/val;
-   }
-   cout << sum << " " << val << endl;
+using bigint = long long int ;
+using vi = vector<int> ;
+using vll = vector<long long int> ;
+
+bigint gcd( bigint a , bigint b ){
+    if( b == 0 )
+        return a ;
+    return gcd( b , a % b ) ;
 }
+int main(){
+    FAST_IO ;
+    int N ; cin >> N ;
+    bigint arr[N+1] , ma = 0 , sum = 0 ;
+    
+    for( int i = 0 ; i < N ; i++ ){
+        cin >> arr[i] ;
+        ma = max( ma , arr[i] ) ;
+    }
+    bigint A = 0 ;
+    for( int i = 0 ; i < N ; i++ ){
+        A = gcd( A , ma - arr[i] ) ;
+        sum = sum + ( ma - arr[i] ) ;
+    }
+    cout << sum / A << " " << A << endl ;
+    
+    return 0 ;
+}
+
+
