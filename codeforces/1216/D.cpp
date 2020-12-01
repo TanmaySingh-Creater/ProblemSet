6639713de1c207970d27b43401bc06b8a68b703f
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-12-01 15:31:00 +0530

    Add solution for problem `D - Swords` Link: https://codeforces.com/contest/1216/problem/D Tags: math, *1300 Ref: https://codeforces.com/contest/1216/submission/100100973

--- codeforces/1216/D.cpp
+++ codeforces/1216/D.cpp
@@ -1,63 +1,25 @@
 #include<bits/stdc++.h>
-
-
-using namespace std;
-
-long long int gcd(vector<long long int>&v)
-{
-	long long int gcd =v[0];
-	for(long long int i=1; i<v.size();i++)
-	gcd = __gcd(gcd,v[i]);
-	
-	
-	return gcd;
-	
-}
-	
-void solve()
-{
-long long int n;
-cin>>n;
-vector<long long int>v(n);
-vector<long long int>v1;
-
-for(long long int i=0; i<n; i++)
-cin>>v[i];
-
-long long int x=0;
-for(long long int i=0; i<n; i++)
-x= max(x,v[i]);
-long long int s=0;
-for(long long int i=0; i<n; i++)
-if(x-v[i]!=0)
-{
-s+= (x-v[i]);
-v1.push_back(x-v[i]);
-
-
-}
-
-long long int k = gcd(v1);
-
-cout<<s/k<<" "<<k;
-
-
-
-
-
-
-
-
-
-}
-int main()
-{
-/*long long int t;
-cin>>t;
-while(t--)
-{
-	solve();
-	cout<<endl;
-}*/
-solve();
+#define ll long long
+#define fastio ios_base::sync_with(false); cin.tie(0);cout.tie(0);
+const int MOD = 1000000007;
+#define PI = acos(-1)
+using namespace std ;
+int main(){
+   int n ;cin >> n ; 
+   vector<int> v(n);
+   int ans = 0 ; 
+   for(int i =0 ; i < n ; i++){
+   	    cin >> v[i];
+   	    ans = max(v[i] ,ans);
+   }
+   int val = 0 ; 
+   for(int i = 0 ; i < n ; i++){
+   	   val = __gcd(val , (ans-v[i]));
+   }
+   int z = val ;
+   ll sum = 0 ; 
+   for(int i = 0 ; i < n ; i++){
+   	   sum += (ans-v[i])/val;
+   }
+   cout << sum << " " << val << endl;
 }
