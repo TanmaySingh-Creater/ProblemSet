00766b164586a3a1986a7d10e0b9d649607f04bf
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-12-01 14:34:00 +0530

    Add solution for problem `A - Prefixes` Link: https://codeforces.com/contest/1216/problem/A Tags: strings, *800 Ref: https://codeforces.com/contest/1216/submission/100097100

--- codeforces/1216/A.cpp
+++ codeforces/1216/A.cpp
@@ -1,41 +1,32 @@
 #include<bits/stdc++.h>
-
-
-using namespace std;
-
-
-	
-void solve()
-{
-int n;
-cin>>n;
-int a[n];
-string s;
-cin>>s;
-int c=0;
-for(int i=0;i<n; i+=2)
-if(s[i]==s[i+1])
-{
-	c++;
-	if(s[i]=='a')
-	s[i]='b'; 
-	else
-	s[i]='a';
-}
-cout<<c<<endl;
-cout<<s;
-
-
-}
-
-int main()
-{
-/*int t;
-cin>>t;
-while(t--)
-{
-	solve();
-	cout<<endl;
-}*/
-solve();
+#define ll long long
+#define fastio ios_base::sync_with(false); cin.tie(0);cout.tie(0);
+const int MOD = 1000000007;
+#define PI = acos(-1)
+using namespace std ;
+int main(){
+    int n ; 
+    cin>> n ; 
+    string s ; cin >> s ;
+    int cnta =0 , cntb=0;
+    for(auto x : s){
+    	if(x=='a') cnta++;
+    	else if(x=='b')cntb++;
+	}
+	int ans = 0 ;
+	for(int i = 0 ; i < n ; i+=2){
+		 if(s[i]!=s[i+1]) continue ;
+		 else {
+		 	if(s[i]=='a'){
+		 		s[i+1] = 'b';
+		 		ans++;
+			 }
+			 else {
+			 	s[i+1]='a';
+			 	ans++;
+			 }
+		 }
+	}
+	cout << ans << endl;
+	cout  << s << endl;
 }
