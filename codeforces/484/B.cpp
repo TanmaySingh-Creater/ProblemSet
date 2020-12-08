880783dc746c74fc0c75053745b9092927fa2c86
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-12-08 22:47:00 +0530

    Add solution for problem `B - Maximum Value` Link: https://codeforces.com/contest/484/problem/B Tags: binary search, math, sortings, two pointers, *2100 Ref: https://codeforces.com/contest/484/submission/100716566

---
+++ codeforces/484/B.cpp
@@ -0,0 +1,44 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+
+#define FAST_IO  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define mod 1000000007
+#define endl "\n"
+#define deb(x) cout << #x << " " << x << endl ;
+#define PI 3.14159265358979323846264338327
+
+
+using namespace std ;
+using bigint = long long int ;
+using vi = vector<int> ;
+using vll = vector<long long int> ;
+
+
+int prev_max[2000005] , arr[2000005] ;
+set<int> s ;
+int main(){
+    FAST_IO ;
+    int N ; cin >> N ;
+    int  ans = 0 , mx = 0 ;
+    for( int i = 0 ; i < N ; i++ ){
+        int x ; cin >> x ;
+        arr[x] = x ;
+        mx = max( mx , 2 * x ) ;
+    }
+    for( int i = 1 ; i <= 2e6 + 1 ; i++ ){
+        prev_max[i] = max( arr[i] , prev_max[i-1] ) ;
+    }
+    
+    for( int i = 0 ; i <= 1e6 ; i++ ){
+        if( arr[i] ){
+            for( int j = 2 * i ; j <= mx ; j += i ){
+                ans = max( ans , prev_max[j-1] % i ) ;
+            }
+        }
+    }
+    
+    cout << ans << endl ;
+    return 0 ;
+}
+
