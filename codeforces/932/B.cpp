589866e0318d5a5f2cd87f7584ed4a6a4eee65c9
Tanmay Singh <theaxiomaticcuber@gmail.com>
2020-12-14 18:12:00 +0530

    Add solution for problem `B - Recursive Queries` Link: https://codeforces.com/contest/932/problem/B Tags: binary search, data structures, dfs and similar, *1300 Ref: https://codeforces.com/contest/932/submission/101169661

--- codeforces/932/B.cpp
+++ codeforces/932/B.cpp
@@ -11,23 +11,20 @@ using namespace std ;
 using bigint = long long int ;
 using vi = vector<int> ;
 using vll = vector<long long int> ;
-int dp[2000002] ;
+
 int generator( int N ){
     if( N < 10 )
         return N ;
-    if( dp[N] != -1 )
-        return dp[N] ;
     int nums = 1 ;
     while( N > 0 ){
         nums = nums * max( 1 , N % 10 ) ;
         N /= 10 ;
     }
-    return dp[N] = generator(nums) ;
+    return generator(nums) ;
 }
 vi adj[10] ;
 int main(){
     NFS ;
-    memset(dp, -1, sizeof(dp)) ;
     for( int i = 1 ; i <= 1e6 ; i++ ){
         int x = generator(i) ;
         adj[x].push_back(i) ;
